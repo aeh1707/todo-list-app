c235d5906fb9bf64810167002ca3cf34e9e1d685
aster-alemu <leaster2014w@gmail.com>
2022-06-22 13:19:03 -0700

    Add more unit test for the add function

--- src/modules/CRUD.test.js
+++ src/modules/CRUD.test.js
@@ -9,6 +9,10 @@ describe('addTask function testing', () => {
     expect(addTask(undefined, [{ description: 'swimming', completed: false, index: 1 }])).toEqual([{ description: 'swimming', completed: false, index: 1 }]);
   });
 
+  test('description swimming index 1 description watch movies index 2', () => {
+    expect(addTask('watch movies', [{ description: 'swimming', completed: false, index: 1 }])).toEqual([{ description: 'swimming', completed: false, index: 1 }, { description: 'watch movies', completed: false, index: 2 }]);
+  });
+
   test('description swimming index 1, description cooking index 2', () => {
     expect(addTask('cooking', [{ description: 'swimming', completed: false, index: 1 }])).toEqual([{ description: 'swimming', completed: false, index: 1 }, { description: 'cooking', completed: false, index: 2 }]);
   });
